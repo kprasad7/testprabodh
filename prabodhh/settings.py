f47ecb8eb3609d3bab2ef66d60aee0b9f4ca4644
kprasad7 <kanuridurgaprasad049@gmail.com>
2021-01-14 02:08:57 +0530

    updating static

--- prabodhh/settings.py
+++ prabodhh/settings.py
@@ -63,6 +63,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'prabodhh.urls'
@@ -160,7 +161,9 @@ EMAIL_RECEIVING_USER = ['jayalaxme24@gmail.com'] # email on which you will recei
 
 
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 STATIC_ROOT = os.path.join(BASE_DIR , 'staticfiles')
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 STATIC_URL = '/static/'
